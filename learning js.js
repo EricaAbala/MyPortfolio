d1496edff35be5efdbc5902eb71f1eefd3804f39
Erica Abala <abalaerica1@gmail.com>
2022-04-20 02:57:58 +0100

    First commit for my  portfolio project

---
+++ learning js.js
@@ -0,0 +1,21 @@
+//scope
+function getaverage(a,b){
+    var average = (a+b)/2;
+    return average;
+  }
+  var result = getaverage(8,4);
+  console.log(`final answer is ${result}`)
+  //array
+  var myArray = new Array();
+  myArray[0] =25;
+  myArray[1] =50;
+  myArray[2] ="true";
+  myArray[3] ="hello";
+  console.log (myArray);
+  
+  myArray.length;
+  myArray.reverse();
+  
+  var friendsArray = ["candy", "edith", "cilia", "esther"];
+  console.log(friendsArray[3]);
+  console.log(friendsArray); 
